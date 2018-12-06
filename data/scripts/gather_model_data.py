02ced2d102fda1dbd5b700ff65eb397fc41b1e80
jregel <jregel@gmail.com>
2018-12-06 09:21:09 -0600

    add fx data gathering

--- data/scripts/gather_model_data.py
+++ data/scripts/gather_model_data.py
@@ -52,5 +52,31 @@ def get_crypto_data(symbol, time='DIGITAL_CURRENCY_DAILY'):
                     writer.writerow(fields)
     return True
 
+def get_fx_data(from_currency, to_currency, time='FX_DAILY'):
+    
+    payload = {'function':time,
+            'from_symbol': from_currency,
+            'to_symbol':to_currency, 
+            'outputsize':'full',
+            'apikey':'V25VXU9H1BTNXDYY'}
+
+    r = requests.get('https://www.alphavantage.co/query?', params=payload)
+    responses = r.json()
+
+    file_dir = os.path.dirname(os.path.realpath('__file__'))
+
+    for name, dictionary in responses.iteritems():
+        print(name)
+        if "Series" in name:
+            for date, data in dictionary.iteritems():
+                print(data)
+                fields=[date, data['1. open'], data['2. high'], data['3. low'],  data['4. close']]
+                with open(os.path.join(file_dir, '../'+from_currency+to_currency+'.csv'), 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(fields)
+    return True
+
+
 if __name__ == '__main__':
     get_crypto_data('BTC')
+    get_fx_data('CNY', 'USD')
